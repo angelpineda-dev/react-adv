9e3573501853b2f856bb457e8c416c08da40f89c
angelpineda-dev <angeljpa95@gmail.com>
2024-05-27 12:40:14 -0600

    routes without lazy load

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, NavLink, Navigate, Route, Routes } from "react-router-do
 
 import logo from '../logo.svg';
 
+import { routes } from './routes';
+
 export const Navigation = () => {
   return (
 		<BrowserRouter>
@@ -9,24 +11,25 @@ export const Navigation = () => {
 				<nav>
 					<img src={logo} alt="React Logo" />
                   <ul>
-                      <li>
-                          <NavLink to="/" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Home</NavLink>
-                      </li>
-                      <li>
-                          <NavLink to="/about">About</NavLink>
-                      </li>
-                      <li>
-                          <NavLink to="/users">Users</NavLink>
-                      </li>
+
+                    {
+                        routes.map(({to, name}) => (
+                        <li key={to}>
+                            <NavLink to={to} className={({ isActive }) => isActive ? 'nav-active' : ''}> {name} </NavLink>
+                        </li>))
+                    }
+
                   </ul>
 				</nav>
 
                 <Routes>
-                  <Route path="about" element={<h1>About Page</h1>} />
-                  <Route path="users" element={<h1>Users Page</h1>} />
-                  <Route path="/" element={<h1>HomePage</h1>} />
 
-                  <Route path="*" element={<Navigate to="/home" replace />} />
+                      {
+                        routes.map( ({to, path, Component}) => (
+                        <Route key={to} path={path} element={<Component/>} />))
+                      }
+
+                  <Route path="*" element={<Navigate to={routes[0].to} replace />} />
                 </Routes>
 				
 			</div>
